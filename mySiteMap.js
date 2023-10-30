d7d62acf7ecf104425712fb1449d392b70c086e7
gaby1986 <gabrieldario.lombardo@gmail.com>
2023-10-30 16:27:07 -0300

    listeners

--- mySiteMap.js
+++ mySiteMap.js
@@ -2,12 +2,75 @@ SalesforceInteractions.init({
     cookieDomain: "cronista.com",
 }).then(() => {
 
+    const urlCronista = 'https://qa.cronista.com/';
+    const dateTime = " | " + new Date().toString()
     //DATOS DE USUARIO
     const email = vsm.session.email;
     const idUser = vsm.session.id;
     const userName = vsm.session.title;
     const isSuscriber = site.session.isSuscriber() ? "Suscriptor" : "Usuario";
-    //BLOQUES DE LA HOME
+
+    //TAGS
+    let edicionImpresaTag = "";
+    let dolarTag = "";
+    let dolarBlueTag = "";
+    let quienEsQuienTag = "";
+    document.querySelectorAll("#main-menu li").forEach(card => {
+        const text = card.querySelector("a span").innerText;
+        switch (text) {
+            case "EDICIÓN IMPRESA":
+                edicionImpresaTag = card.querySelector("a span");
+                break;
+            case "DÓLAR":
+                dolarTag = card.querySelector("a span");
+                break;
+            case "DÓLAR BLUE":
+                dolarBlueTag = card.querySelector("a span");
+                break;
+            case "QUIÉN ES QUIÉN":
+                quienEsQuienTag = card.querySelector("a span");
+                break;
+        }
+    });
+    let subMenuEdicionImpresaTag = "";
+    let subMenuQuienEsQuienTag = "";
+    document.querySelectorAll("#more-list li").forEach(card => {
+        const text = card.querySelector("a span").innerText;
+        console.log(text)
+        switch (text) {
+            case "EDICIÓN IMPRESA":
+                subMenuEdicionImpresaTag = card.querySelector("a span");
+                break;
+            case "QUIÉN ES QUIÉN":
+                subMenuQuienEsQuienTag = card.querySelector("a span");
+                break;
+        }
+    });
+
+    //Tickers
+    let dolarBlueTicker = "";
+    let dolarBnaTicker = "";
+    let dolarMepTicker = "";
+    let dolarTarjetaTicker = "";
+    document.querySelectorAll("#market-scrll-1 li").forEach(card => {
+        const text = card.querySelector("a span").innerText;
+        switch (text) {
+            case "DÓLAR BLUE":
+                dolarBlueTicker = card.querySelector("a span");
+                break;
+            case "DÓLAR BNA":
+                dolarBnaTicker = card.querySelector("a span");
+                break;
+            case "DÓLAR MEP":
+                dolarMepTicker = card.querySelector("a span");
+                break;
+            case "DÓLAR TARJETA":
+                dolarTarjetaTicker = card.querySelector("a span");
+                break;
+        }
+    });
+
+    //SELECTORES DE LA HOME
     const main = document.querySelector('.main-container');
     const block = main.querySelectorAll('div.block');
     const ulOptions = document.querySelector('.session-options');
@@ -35,6 +98,7 @@ SalesforceInteractions.init({
                 { name: "Footer", selector: '#page-footer' },
             ],
             listeners: [
+                //MENU HEADER
                 SalesforceInteractions.listener("click", '#page-header-session-box .sign-in-button', () => {
                     SalesforceInteractions.sendEvent({
                         interaction: { name: SalesforceInteractions.cashDom('#page-header-session-box .sign-in-button').text(), },
@@ -44,6 +108,98 @@ SalesforceInteractions.init({
                     SalesforceInteractions.sendEvent({
                         interaction: {
                             name: SalesforceInteractions.cashDom(`#page-header-middle .b-suscription`).text(),
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `.svg-icon.menu`, (e) => {
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Botón menu" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${perfil.tagName}`, (e) => {
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Botón perfil" + dateTime,
+                        },
+                    });
+                }),
+                //TAGS           
+                SalesforceInteractions.listener("click", `${dolarTag.tagName}`, (e) => {
+                    console.log("dolar BNA")
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Dólar BNA tag" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${dolarBlueTag.tagName}`, (e) => {
+                    console.log("dolar blue")
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Dólar blue tag" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${edicionImpresaTag.tagName}`, (e) => {
+                    console.log("edicionImpresaTag")
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Edición impresa tag" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${quienEsQuienTag.tagName}`, (e) => {
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Quien es quien tag" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${subMenuEdicionImpresaTag.tagName}`, (e) => {
+                    console.log("edicionImpresaTag")
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Edición impresa tag" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${subMenuQuienEsQuienTag.tagName}`, (e) => {
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Quien es quien tag" + dateTime,
+                        },
+                    });
+                }),
+                //TICKERS
+                SalesforceInteractions.listener("click", `${dolarBlueTicker.tagName}`, (e) => {
+                    console.log("dolar blue ticker")
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Dólar blue ticker" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${dolarBnaTicker.tagName}`, (e) => {
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Dólar BNA ticker" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${dolarMepTicker.tagName}`, (e) => {
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Dólar mep ticker" + dateTime,
+                        },
+                    });
+                }),
+                SalesforceInteractions.listener("click", `${dolarTarjetaTicker.tagName}`, (e) => {
+                    console.log("dolarTarjetaTicker")
+                    SalesforceInteractions.sendEvent({
+                        interaction: {
+                            name: "Dólar tarjeta ticker" + dateTime,
                         },
                     });
                 }),
@@ -64,7 +220,7 @@ SalesforceInteractions.init({
                     if (url.includes('?')) {
                         url = url.slice(0, window.location.href.lastIndexOf('?'));
                     }
-                    let isMatch = (url === 'https://dev.cronista.com/' ? true : false);
+                    let isMatch = (url === urlCronista ? true : false);
 
                     return isMatch;
                 },
@@ -116,7 +272,7 @@ SalesforceInteractions.init({
                     SalesforceInteractions.listener("click", `.${block[1].className} h2.title`, () => {
                         SalesforceInteractions.sendEvent({
                             interaction: {
-                                name: "Article: " + SalesforceInteractions.cashDom(this).text(),
+                                name: "Article: " + SalesforceInteractions.cashDom(this).text() + dateTime,
                             },
                             user: {
                                 identities: {
@@ -131,32 +287,17 @@ SalesforceInteractions.init({
                     SalesforceInteractions.listener("click", `.locked a`, (e) => {
                         SalesforceInteractions.sendEvent({
                             interaction: {
-                                name: SalesforceInteractions.cashDom(e.target).text(),
+                                name: SalesforceInteractions.cashDom(e.target).text() + dateTime,
                             },
                         });
                     }),
                     SalesforceInteractions.listener("click", `.columnists .items article.item`, (e) => {
                         SalesforceInteractions.sendEvent({
                             interaction: {
-                                name: SalesforceInteractions.cashDom(e.target).text(),
+                                name: SalesforceInteractions.cashDom(e.target).text() + dateTime,
                             },
                         });
                     }),
-                    SalesforceInteractions.listener("click", `.svg-icon.menu`, (e) => {
-                        SalesforceInteractions.sendEvent({
-                            interaction: {
-                                name: "Botón menu",
-                            },
-                        });
-                    }),
-                    SalesforceInteractions.listener("click", `${perfil.tagName}`, (e) => {
-                        SalesforceInteractions.sendEvent({
-                            interaction: {
-                                name: "Botón menu",
-                            },
-                        });
-                    }),
-
                 ]
             },
         ]
