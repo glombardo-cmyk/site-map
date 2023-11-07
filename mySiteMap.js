4a1f6f2bddc2cd2f09e141e769a77c20c2af523d
gaby1986 <gabrieldario.lombardo@gmail.com>
2023-11-07 12:39:54 -0300

    main global

--- mySiteMap.js
+++ mySiteMap.js
@@ -13,8 +13,8 @@ SalesforceInteractions.init({
     const isSuscriber = site.session.isSuscriber() && site.session != undefined ? "Suscriptor" : "Usuario";
 
 
-    let main = ""
-    let block = ""
+    let main = document.querySelector('.main-container');
+    let block = main.querySelectorAll('div.block');
     let isMatch = false
     let url = ""
     //SELECTORES DE LA Genericos
@@ -96,13 +96,13 @@ SalesforceInteractions.init({
                     });
                 }),
                 //TAGS
-                SalesforceInteractions.listener("click", `#main-menu li a span`, (e) => {
+                SalesforceInteractions.listener("click", `#main-menu li a span.text`, (e) => {
                     switch (e.target.innerText) {
                         case "EDICIÓN IMPRESA":
                             console.log(e.target.innerText)
                             SalesforceInteractions.sendEvent({
                                 interaction: {
-                                    name: e.target.innerText,
+                                    name: "TAG: " + e.target.innerText,
                                 },
                             });
                             break;
@@ -110,7 +110,7 @@ SalesforceInteractions.init({
                             console.log(e.target.innerText)
                             SalesforceInteractions.sendEvent({
                                 interaction: {
-                                    name: e.target.innerText,
+                                    name: "TAG: " + e.target.innerText,
                                 },
                             });
                             break;
@@ -118,7 +118,7 @@ SalesforceInteractions.init({
                             console.log(e.target.innerText)
                             SalesforceInteractions.sendEvent({
                                 interaction: {
-                                    name: e.target.innerText,
+                                    name: "TAG: " + e.target.innerText,
                                 },
                             });
                             break;
@@ -126,7 +126,7 @@ SalesforceInteractions.init({
                             console.log(e.target.innerText)
                             SalesforceInteractions.sendEvent({
                                 interaction: {
-                                    name: e.target.innerText,
+                                    name: "TAG: " + e.target.innerText,
                                 },
                             });
                             break;
@@ -139,28 +139,28 @@ SalesforceInteractions.init({
                         case "DÓLAR BLUE":
                             SalesforceInteractions.sendEvent({
                                 interaction: {
-                                    name: e.target.innerText,
+                                    name: "TICKER: " + e.target.innerText,
                                 },
                             });
                             break;
                         case "DÓLAR BNA":
                             SalesforceInteractions.sendEvent({
                                 interaction: {
-                                    name: e.target.innerText,
+                                    name: "TICKER: " + e.target.innerText,
                                 },
                             });
                             break;
                         case "DÓLAR MEP":
                             SalesforceInteractions.sendEvent({
                                 interaction: {
-                                    name: e.target.innerText,
+                                    name: "TICKER: " + e.target.innerText,
                                 },
                             });
                             break;
                         case "DÓLAR TARJETA":
                             SalesforceInteractions.sendEvent({
                                 interaction: {
-                                    name: e.target.innerText,
+                                    name: "TICKER: " + e.target.innerText,
                                 },
                             });
                             break;
@@ -183,10 +183,7 @@ SalesforceInteractions.init({
                         url = url.slice(0, window.location.href.lastIndexOf('?'));
                     }
                     isMatch = (url === `${enviroment}.cronista.com` ? true : false);
-                    if (isMatch) {
-                        main = document.querySelector('.main-container');
-                        block = main.querySelectorAll('div.block');
-                    }
+
 
                     return isMatch;
                 },
@@ -209,8 +206,9 @@ SalesforceInteractions.init({
                         emailAddress: email,
                     },
                 },
-                listeners: isMatch ? [
+                listeners: [
                     SalesforceInteractions.listener("click", `.${block[0].className} h2.title`, (e) => {
+                        console.log(SalesforceInteractions.cashDom(e.target).text())
                         SalesforceInteractions.sendEvent({
                             interaction: {
                                 name: "Article: " + SalesforceInteractions.cashDom(e.target).text(),
@@ -225,7 +223,8 @@ SalesforceInteractions.init({
                             }
                         });
                     }),
-                    SalesforceInteractions.listener("click", `.${block[1].className} h2.title`, () => {
+                    SalesforceInteractions.listener("click", `.${block[1].className} h2.title`, (e) => {
+                        console.log(SalesforceInteractions.cashDom(e.target).text())
                         SalesforceInteractions.sendEvent({
                             interaction: {
                                 name: "Article from home: " + SalesforceInteractions.cashDom(e.target).text(),
@@ -241,91 +240,17 @@ SalesforceInteractions.init({
                         });
                     }),
                     SalesforceInteractions.listener("click", `.locked a`, (e) => {
+                        console.log(SalesforceInteractions.cashDom(e.target).text())
                         SalesforceInteractions.sendEvent({
                             interaction: {
-                                name: SalesforceInteractions.cashDom(e.target).text(),
+                                name: "Article Member from home: " + SalesforceInteractions.cashDom(e.target).text(),
                             },
                         });
                     }),
                     SalesforceInteractions.listener("click", `.columnists .items article.item`, (e) => {
                         SalesforceInteractions.sendEvent({
                             interaction: {
-                                name: SalesforceInteractions.cashDom(e.target).text(),
-                            },
-                        });
-                    }),
-                ] : []
-            },
-            {
-                name: "Page PayWall",
-                isMatch: () => {
-                    url = window.location.href;
-
-                    if (url.includes('?')) {
-                        url = url.slice(0, window.location.href.lastIndexOf('?'));
-                    }
-
-                    isMatch = (url === `${enviroment}.cronista.com/suscripciones/` ? true : false);
-
-                    return isMatch;
-                },
-                interaction: {
-                    name: "Page Pay Wall",
-                    action: "Ingresa a suscripciones",
-                    attributes: {
-                        userIdCms: idUser,
-                        name: userName,
-                        isSuscriber: isSuscriber,
-                        emailAddress: email,
-                        continueUrl: url
-                    },
-                },
-                onActionEvent: (actionEvent) => {
-                    if (email) {
-                        let limit = new URL(window.location.href).searchParams.get("limit")
-                        let continueUrl = new URL(window.location.href).searchParams.get("continue")
-                        actionEvent.user = actionEvent.user || {};
-                        actionEvent.user.attributes = actionEvent.user.attributes || {};
-                        actionEvent.user.attributes.typeOfEntry = limit ? "choque metered" : "Choque directo";
-                        actionEvent.user.attributes.continueUrl = continueUrl;
-                    }
-                    return actionEvent;
-                },
-                listeners: [
-                    //Boton quiero suscribirme
-                    SalesforceInteractions.listener("click", `.item .button`, (e) => {
-                        const cardTitle = e.currentTarget.parentNode.querySelector('h2.title');
-                        SalesforceInteractions.sendEvent({
-                            interaction: {
-                                name: "Quiero suscribirme " + SalesforceInteractions.cashDom(cardTitle).text(),
-                            },
-                        });
-                    }),
-                    //Boton detalles
-                    SalesforceInteractions.listener("click", `.item .list_items`, (e) => {
-                        const cardTitle = e.currentTarget.parentNode.querySelector('h2.title');
-                        SalesforceInteractions.sendEvent({
-                            interaction: {
-                                name: "Detalle " + SalesforceInteractions.cashDom(cardTitle).text(),
-                            },
-                        });
-                    }),
-                    //Boton otros planes
-                    SalesforceInteractions.listener("click", `.otros-planes__item a`, (e) => {
-                        const linkTitle = e.currentTarget.parentNode.querySelector('span');
-                        SalesforceInteractions.sendEvent({
-                            interaction: {
-                                name: "Otros planes " + SalesforceInteractions.cashDom(linkTitle).text(),
-                            },
-                        });
-                    }),
-                    //Link terminos y condiciones | Precios vigentes
-                    SalesforceInteractions.listener("click", `.footer-footer a`, (e) => {
-                        const linkTitle = e.currentTarget;
-                        console.log(SalesforceInteractions.cashDom(linkTitle).text())
-                        SalesforceInteractions.sendEvent({
-                            interaction: {
-                                name: SalesforceInteractions.cashDom(linkTitle).text(),
+                                name: "Columnists: " + SalesforceInteractions.cashDom(e.target).text(),
                             },
                         });
                     }),
